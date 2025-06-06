dfacb49af8a26c74637b1e742165d6bbf6cfff31
Dimalsha Perera <139336872+DimalshaPerera@users.noreply.github.com>
2025-06-06 22:54:40 +0530

    initialized profile btn

--- Day4Activity/app/src/main/java/com/example/day4activity/MainActivity.kt
+++ Day4Activity/app/src/main/java/com/example/day4activity/MainActivity.kt
@@ -8,14 +8,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.day4activity.databinding.ActivityMainBinding
+import com.example.day4activity.databinding.BottomNavBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var bottomNavBinding:BottomNavBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        bottomNavBinding = BottomNavBinding.bind(binding.root.findViewById(R.id.bottom_nav_include))
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
 
@@ -42,6 +45,10 @@ class MainActivity : AppCompatActivity() {
             }
             binding.drawerLayout.closeDrawers()
             true
+        }
+
+        bottomNavBinding.profile.setOnClickListener {
+            startActivity(Intent(this, Profile::class.java))
         }
 
     }
---
+++ Day4Activity/app/src/main/java/com/example/day4activity/Profile.kt
@@ -0,0 +1,24 @@
+package com.example.day4activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.day4activity.databinding.ActivityMainBinding
+import com.example.day4activity.databinding.ActivityProfileBinding
+
+class Profile : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProfileBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+
+
+    }
+}
