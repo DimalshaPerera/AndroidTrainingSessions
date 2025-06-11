cdc1b185059e404b00c4e57abb82a2285a0a8acd
Dimalsha Perera <139336872+DimalshaPerera@users.noreply.github.com>
2025-06-11 13:01:18 +0530

    created a new activtiy

--- FoodApp/app/src/main/java/com/example/foodapp/MainActivity.kt
+++ FoodApp/app/src/main/java/com/example/foodapp/MainActivity.kt
@@ -25,5 +25,10 @@ class MainActivity : AppCompatActivity() {
             val intent= Intent(this,Home::class.java)
             startActivity(intent)
         }
+
+        binding.listButton.setOnClickListener{
+            val intent= Intent(this,ListView::class.java)
+            startActivity(intent)
+        }
     }
 }
